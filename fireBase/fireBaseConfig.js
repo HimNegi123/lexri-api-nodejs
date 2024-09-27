db576a8f483eaaa113f22df30327aef469b37926
Himanshu Negi <87644526+HimNegi123@users.noreply.github.com>
2024-09-27 20:11:39 +0530

    mirror changes

--- fireBase/fireBaseConfig.js
+++ fireBase/fireBaseConfig.js
@@ -1,13 +1,13 @@
 // Import the functions you need from the SDKs you need
 const { initializeApp } = require("firebase/app");
 const { getFirestore } = require("firebase/firestore");
-
+require("dotenv").config(); 
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
-  apiKey: "AIzaSyACzIInHPTPc8HJ4FNVGfGFXv7teYD-45k",
+  apiKey: process.env.FIRE_KEY,
   authDomain: "demotesting-7a8b8.firebaseapp.com",
   projectId: "demotesting-7a8b8",
   storageBucket: "demotesting-7a8b8.appspot.com",
